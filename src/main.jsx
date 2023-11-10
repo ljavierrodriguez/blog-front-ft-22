5e276fd6d030094f98341b53043743f21a6be7e3
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2023-11-10 10:46:58 -0300

    Base Route Home

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+
+import App from './App'
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(<App />)
